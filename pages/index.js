e3a4b70121b8e3340e38e6517998cc4c2eafe53a
Justyboy <41842024+Justyboy@users.noreply.github.com>
2021-08-20 16:49:20 -0400

    End of day push

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
@@ -22,6 +22,12 @@ export default function Home() {
     <main className="max-w-7xl mx-auto px-8 sm:px-16">
       <section className="pt-6">
         <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+        {/* Pull data from a server API endpoints */}
+        {exploreData?.map((item) => (
+          <h1>{item.location}</h1>
+
+        ))}
       </section>
     </main>
     </div>
@@ -30,5 +36,14 @@ export default function Home() {
 
 
 export async function getStaticProps(){
-  
+  const exploreData= await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+  return {
+    props:{
+      exploreData
+      
+    }
+  }
 }
+
