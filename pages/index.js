f9e9fae00d58b58891527f9495516be7c58a7e42
Justyboy <41842024+Justyboy@users.noreply.github.com>
2021-08-20 16:28:08 -0400

    start of API

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,19 @@ export default function Home() {
 
       {/* Banner */}
     <Banner/>
+
+    {/* Main */}
+
+    <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <section className="pt-6">
+        <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+      </section>
+    </main>
     </div>
   )
+}
+
+
+export async function getStaticProps(){
+  
 }
