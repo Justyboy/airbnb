7d0b79264e86b1f7ede7c08d3ca14e486113c70a
Justin <justyboy27@gmail.com>
2021-08-21 23:35:39 -0400

    ADD: cancel button under calendar

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,7 @@ function Header() {
   const [searchInput, setSearchInput] =  useState("");
   const [startDate, setStartDate] = useState(new Date())
   const [endDate, setEndDate] = useState(new Date())
+  const [noOfGuest, setNoOfGuest] = useState(1)
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate)
@@ -22,6 +23,9 @@ function Header() {
     key: 'selection'
   }
   
+  const resetInput =() => {
+    setSearchInput("")
+  }
 
   return (
     <header className="sticky top-0 z-50 grid
@@ -56,13 +60,26 @@ function Header() {
       </div>
 
 {searchInput && (
- <div>
+ <div className="flex flex-col col-span-3 mx-auto">
    <DateRangePicker ranges={[selectionRange]} 
    minDate={new Date()}
    rangeColors={["#FD5861"]}
    onChange={handleSelect}
    />
- </div>
+ <div className="flex items-center border-b mb-4">
+   <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+   <UsersIcon className="h-5"/>
+   <input value={noOfGuest} 
+   onChange={(e) => setNoOfGuest(e.target.value)}
+    type="number"
+    min={1}
+    className="w-12 pl-2 text-lg outline-none text-red-400"/>
+   </div>
+   <div className="flex">
+     <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
+     <button className="flex-grow text-red-500">Search</button>
+   </div>
+   </div>
 )}
    
     </header>
